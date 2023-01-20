2d3b875c17674b923ae278d07240afed61a24ccd
CheeseWolt <benoit.routier7@gmail.com>
2023-01-20 16:02:43 +0100

    Ajout de la barre de navigation

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,26 +1,142 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import {
+  createStyles,
+  Header,
+  Group,
+  Button,
+  Divider,
+  Box,
+  Burger,
+  Drawer,
+  ScrollArea,
+} from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 
-import styles from "./menu.module.css"
+import Image from 'next/image';
+import Link from 'next/link';
 
-const Menu = () => {
+const useStyles = createStyles((theme) => ({
+  link: {
+    display: 'flex',
+    alignItems: 'center',
+    height: '100%',
+    paddingLeft: theme.spacing.md,
+    paddingRight: theme.spacing.md,
+    textDecoration: 'none',
+    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+    fontWeight: 500,
+    fontSize: theme.fontSizes.sm,
+
+    [theme.fn.smallerThan('sm')]: {
+      height: 42,
+      display: 'flex',
+      alignItems: 'center',
+      width: '100%',
+    },
+
+    ...theme.fn.hover({
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+    }),
+  },
+
+  subLink: {
+    width: '100%',
+    padding: `${theme.spacing.xs}px ${theme.spacing.md}px`,
+    borderRadius: theme.radius.md,
+
+    ...theme.fn.hover({
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[0],
+    }),
+
+    '&:active': theme.activeStyles,
+  },
+
+  dropdownFooter: {
+    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[0],
+    margin: -theme.spacing.md,
+    marginTop: theme.spacing.sm,
+    padding: `${theme.spacing.md}px ${theme.spacing.md * 2}px`,
+    paddingBottom: theme.spacing.xl,
+    borderTop: `1px solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1]
+    }`,
+  },
+
+  hiddenMobile: {
+    [theme.fn.smallerThan('sm')]: {
+      display: 'none',
+    },
+  },
+
+  hiddenDesktop: {
+    [theme.fn.largerThan('sm')]: {
+      display: 'none',
+    },
+  },
+}));
+
+export default function Menu() {
+  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
+  const { classes, theme } = useStyles();
+
   return (
-    <nav className={styles.topnav}>
-        <Link href={"/"} className={styles.logo}>
-            <Image
-                src="/images/localib-logo.png"
-                alt="Localib"
-                width={128}
-                height={128}
-            />
-        </Link>
-        <button className={styles.button}>Connexion</button>
-        <Link href="/about" className={styles.link}>A propos</Link>
-        <Link href="/locations" className={styles.link}>Locations</Link>
-        <Link href="/vehicules" className={styles.link}>Véhicules</Link>
-    </nav>
-  )
-}
+    <Box pb={120}>
+      <Header height={128} px="md">
+        <Group position="apart" sx={{ height: '100%' }}>
+          <Link href="/">
+            <Image src={"/images/localib-logo.png"} alt={"localib"} width={128} height={128}/>
+          </Link>
 
-export default Menu
+          <Group sx={{ height: '100%' }} spacing={0} className={classes.hiddenMobile}>
+            <Link href="/vehicules" className={classes.link}>
+              Vehicules
+            </Link>
+            <Link href="locations" className={classes.link}>
+              Locations
+            </Link>
+            <Link href="/about" className={classes.link}>
+              A propos
+            </Link>
+          </Group>
+
+          <Group className={classes.hiddenMobile}>
+            <Button variant="default">Log in</Button>
+            <Button>Sign up</Button>
+          </Group>
+
+          <Burger opened={drawerOpened} onClick={toggleDrawer} className={classes.hiddenDesktop} />
+        </Group>
+      </Header>
+
+      <Drawer
+        opened={drawerOpened}
+        onClose={closeDrawer}
+        size="100%"
+        padding="md"
+        title="Navigation"
+        className={classes.hiddenDesktop}
+        zIndex={1000000}
+      >
+        <ScrollArea sx={{ height: 'calc(100vh - 60px)' }} mx="-md">
+          <Divider my="sm" color={theme.colorScheme === 'dark' ? 'dark.5' : 'gray.1'} />
+
+          <Link href="/vehicules" className={classes.link}>
+            Véhicules
+          </Link>
+          <Link href="/locations" className={classes.link}>
+            Locations
+          </Link>
+          <Link href="/about" className={classes.link}>
+            A propos
+          </Link>
+
+          <Divider my="sm" color={theme.colorScheme === 'dark' ? 'dark.5' : 'gray.1'} />
+
+          <Group position="center" grow pb="xl" px="md">
+            <Button variant="default">Log in</Button>
+            <Button>Sign up</Button>
+          </Group>
+        </ScrollArea>
+      </Drawer>
+    </Box>
+  );
+}
